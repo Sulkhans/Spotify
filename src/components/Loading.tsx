b54ec9cc776ff50f795aa0734146a7daf38cca90
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-03-30 14:58:49 +0400

    Loading

---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+export default function Loading() {
+  return (
+    <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+      <div className="load">
+        <div className="progress"></div>
+        <div className="progress"></div>
+        <div className="progress"></div>
+      </div>
+    </div>
+  );
+}

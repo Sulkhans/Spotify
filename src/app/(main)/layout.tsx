5880e5ce1483c8a0ad55543fac58eeb4ae661085
Sulkhan <126507578+Sulkhans@users.noreply.github.com>
2024-04-06 15:38:50 +0400

    Header

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,15 +1,19 @@
 "use client";
-import { getToken } from "@/utils/cookie";
+import { useRouter } from "next/navigation";
+import { getToken, removeToken } from "@/utils/cookie";
 import Player from "@/components/Player";
 import Sidebar from "@/components/Sidebar";
 import { UserType } from "@/utils/types";
 import { useEffect, useState } from "react";
+import Arrow from "@/assets/arrow.svg";
+import Link from "next/link";
 
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const route = useRouter();
   const token = getToken();
   const [user, setUser] = useState<UserType | null>(null);
 
@@ -31,9 +35,67 @@ export default function MainLayout({
     <div className="w-screen h-[100dvh] p-2 flex flex-col gap-2 bg-black">
       <div className="flex gap-2 h-[calc(100%-80px)]">
         <Sidebar token={token} user={user} />
-        <main className="w-full">{children}</main>
+        <div className="w-full rounded-lg bg-spotify-base">
+          <header className="flex justify-between py-4 px-5">
+            <div>
+              <button
+                onClick={() => route.back()}
+                className="size-8 p-2 mr-2 rounded-full bg-[rgba(0,0,0,.7)]"
+              >
+                <Arrow className="fill-white" />
+              </button>
+              <button
+                onClick={() => route.forward()}
+                className="size-8 p-2 rounded-full bg-[rgba(0,0,0,.7)]"
+              >
+                <Arrow className="fill-white rotate-180" />
+              </button>
+            </div>
+            {user ? (
+              <button className="flex justify-center items-center size-8 rounded-full bg-[rgba(0,0,0,.7)] relative group">
+                <div
+                  style={{ backgroundImage: `url(${user?.image})` }}
+                  className="size-6 bg-center bg-cover rounded-full"
+                />
+                <div className="absolute top-10 right-0 hidden rounded w-32 p-1 text-sm bg-[#282828] group-focus-within:flex flex-col">
+                  <a
+                    href="https://www.spotify.com/account/overview/"
+                    className="p-2.5 rounded text-start hover:bg-[#3e3e3e]"
+                  >
+                    Account
+                  </a>
+                  <button
+                    onClick={() => {
+                      removeToken();
+                      route.replace("/");
+                    }}
+                    className="p-2.5 rounded text-start hover:bg-[#3e3e3e]"
+                  >
+                    Log out
+                  </button>
+                </div>
+              </button>
+            ) : (
+              <div>
+                <a
+                  href="https://www.spotify.com/signup"
+                  className="font-bold text-spotify-gray hover:text-white p-2 mr-6"
+                >
+                  Sign up
+                </a>
+                <Link
+                  href={"/"}
+                  className="px-6 py-2 rounded-full font-bold text-black bg-white hover:scale-105"
+                >
+                  Log in
+                </Link>
+              </div>
+            )}
+          </header>
+          <main>{children}</main>
+        </div>
       </div>
-      <Player />
+      <Player token={token} />
     </div>
   );
 }
